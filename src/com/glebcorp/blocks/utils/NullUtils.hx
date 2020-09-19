821a7befbc30ed13ab8e888e8e4f439278d2b128
glebbash <glebbash@gmail.com>
2020-09-19 20:22:56 +0300

    Rename Unwrap to NullUtils

--- src/com/glebcorp/blocks/utils/NullUtils.hx
+++ src/com/glebcorp/blocks/utils/NullUtils.hx
@@ -2,12 +2,16 @@ package com.glebcorp.blocks.utils;
 
 import com.glebcorp.blocks.utils.Panic.panic;
 
-extern class Unwrap {
+extern class NullUtils {
 	public static inline function unwrap<T>(value: Null<T>): T {
 		return value == null ? panic("Attempt to unwrap null") : (value: T);
 	}
 
 	public static inline function unsafe<T>(value: Null<T>): T {
 		return @:nullSafety(Off) (value: T);
+	}
+
+	public static inline function or<T>(value: Null<T>, def: T): T {
+		return value == null ? def : (value: T);
 	}
 }

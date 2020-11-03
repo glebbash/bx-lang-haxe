1dcae46ffa49969da6de540c86fad9eda483add3
glebbash <glebbash@gmail.com>
2020-11-03 14:23:58 +0200

    Change import syntax

--- src/com/glebcorp/blocks/syntax/Import.hx
+++ src/com/glebcorp/blocks/syntax/Import.hx
@@ -7,32 +7,57 @@ import com.glebcorp.blocks.engine.Prelude;
 import com.glebcorp.blocks.syntax.Identifier;
 import com.glebcorp.blocks.syntax.Object;
 
+using com.glebcorp.blocks.utils.Slice;
+using com.glebcorp.blocks.utils.TokenText;
+using com.glebcorp.blocks.utils.NullUtils;
+
 @:tink class Import implements Atom {
 	final object: Object = _;
 	final ident: Identifier = _;
 
 	function parse(parser: ExprParser, token: Token) {
-		var path = "";
-		while (true) {
-			final token = parser.expect({type: TokenType.Identifier});
-			switch (token.value) {
-				case Text(str):
-					path += str;
-					if (parser.nextIs({value: "::"})) {
-						parser.next();
-						break;
-					}
-					parser.expect({value: "."});
-					path += ".";
-				default:
-					parser.unexpectedToken(token);
-			}
-		}
+		var pairs: Null<Array<KVPair>> = null;
+		var moduleName: Null<String> = null;
 		if (parser.nextIs({type: TokenType.BlockBrace})) {
 			// TODO: check object
-			return new ImportExpr(path, object.parse(parser, parser.next()).pairs);
+			pairs = object.parse(parser, parser.next()).pairs;
+		} else {
+			moduleName = ident.expect(parser).name;
 		}
-		return new ImportExpr(path, [new KVPair(ident.expect(parser).name, null)]);
+
+		parser.expect({ value: "from" });
+
+		final pathToken = parser.expect({ type: TokenType.String });
+		final path = parser.text(pathToken).slice(1, -1);
+
+		return pairs == null
+			? new FullImportExpr(path, moduleName.unsafe())
+			: new ImportExpr(path, pairs);
+	}
+}
+
+@:tink class FullImportExpr implements Expression {
+	final path: String = _;
+	final moduleName: String = _;
+
+	function eval(ctx: Context) {
+		final importCtx = Core.subContext(ctx);
+		importCtx.scope.exports = new Set();
+
+		ctx.core.evalFile(path, importCtx);
+
+		final module = new BObject();
+		for (member in importCtx.scope.exports.unsafe().keys()) {
+			module.set(member, importCtx.scope.get(member));
+		}
+		
+		ctx.scope.define(moduleName, module);
+
+		return BVoid.VALUE;
+	}
+
+	function toString(s = "", i = "") {
+		return 'import $moduleName from "$path"';
 	}
 }
 
@@ -49,11 +74,11 @@ import com.glebcorp.blocks.syntax.Object;
 		for (pair in pairs) {
 			ctx.scope.define(pair.getDef(), importCtx.scope.get(pair.name), true);
 		}
-		// TODO: handle varargs import
+
 		return BVoid.VALUE;
 	}
 
 	function toString(s = "", i = "") {
-		return 'import $path::{${pairs.map(p -> p.name).join(", ")}}';
+		return 'import {${pairs.map(p -> p.name).join(", ")}} from "$path"';
 	}
 }
